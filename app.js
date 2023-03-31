a1fdc6444e0521adc8c5135db2fbbf620ffbdc32
umeshgrg29 <umeshgrg29@gmail.com>
2023-03-31 08:24:41 +0530

    daily expense app with user authentication

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
-
-const sequelize = require('./util/database');
 var cors = require('cors')
-
-const app = express();
-
-app.use(cors())
+const sequelize = require('./util/database');
+const User = require('./models/users');
+const Expense = require('./models/expenses');
 
 const userRoutes = require('./routes/user') 
 const expenseRoutes = require('./routes/expense')
 
+const app = express();
 
-app.use(bodyParser.json({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.json());  //this is for handling jsons
+app.use(cors())
+app.use(express.json()); 
 
 app.use('/user', userRoutes)
 app.use('/expense', expenseRoutes)
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
 
 
 sequelize
