06c385b1ab0531c83e83de74b369510e3cd2a9d5
umeshgrg29 <umeshgrg29@gmail.com>
2023-04-04 12:53:20 +0530

    final leaderboard optimisation

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,44 +1,55 @@
 const Expense = require('../models/expenses');
+const User = require('../models/users')
 
 const addexpense = (req, res) => {
     const { expenseamount, description, category } = req.body;
 
-    if(expenseamount == undefined || expenseamount.length === 0 ){
-        return res.status(400).json({success: false, message: 'Parameters missing'})
+    if (expenseamount == undefined || expenseamount.length === 0) {
+        return res.status(400).json({ success: false, message: 'Parameters missing' })
     }
-    
-    Expense.create({ expenseamount, description, category, userId: req.user.id}).then(expense => {
-        return res.status(201).json({expense, success: true } );
-    }).catch(err => {
-        return res.status(500).json({success : false, error: err})
+
+    Expense.create({ expenseamount, description, category, userId: req.user.id }).then(expense => {
+        const totalExpense = Number(req.user.totalExpenses) + Number(expenseamount)
+        User.update({
+            totalExpenses: totalExpense
+        }, {
+            where: { id: req.user.id }
+        }).then(async() => {
+            res.status(200).json({ expense: expense })
+        })
+        .catch(async (err) => {
+                return res.status(500).json({ success: false, error: err })
+        })
+    }).catch(async (err) => {
+        return res.status(500).json({ success: false, error: err })
     })
 }
 
-const getexpenses = (req, res)=> {
-    
-    Expense.findAll({ where : { userId: req.user.id}}).then(expenses => {
+const getexpenses = (req, res) => {
+
+    Expense.findAll({ where: { userId: req.user.id } }).then(expenses => {
         // console.log('>>>>>>>>>>>>>>inside expense', expenses)
-        return res.status(200).json({expenses, success: true})
+        return res.status(200).json({ expenses, success: true })
     })
-    .catch(err => {
-        console.log(err)
-        return res.status(500).json({ error: err, success: false})
-    })
+        .catch(err => {
+            console.log(err)
+            return res.status(500).json({ error: err, success: false })
+        })
 }
 
 const deleteexpense = (req, res) => {
     const expenseid = req.params.expenseid;
-    if(expenseid == undefined || expenseid.length === 0){
-        return res.status(400).json({success: false, })
+    if (expenseid == undefined || expenseid.length === 0) {
+        return res.status(400).json({ success: false, })
     }
-    Expense.destroy({where: { id: expenseid, userId: req.user.id }}).then((noofrows) => {
-        if(noofrows === 0){
-            return res.status(404).json({success: false, message: 'Expense doenst belong to the user'})
+    Expense.destroy({ where: { id: expenseid, userId: req.user.id } }).then((noofrows) => {
+        if (noofrows === 0) {
+            return res.status(404).json({ success: false, message: 'Expense doenst belong to the user' })
         }
-        return res.status(200).json({ success: true, message: "Deleted Successfuly"})
+        return res.status(200).json({ success: true, message: "Deleted Successfuly" })
     }).catch(err => {
         console.log(err);
-        return res.status(500).json({ success: true, message: "Failed"})
+        return res.status(500).json({ success: true, message: "Failed" })
     })
 }
 
--- controllers/premiumFeature.js
+++ controllers/premiumFeature.js
@@ -6,15 +6,8 @@ const e = require('express');
 const getUserLeaderBoard = async (req, res) => {
     try {
         const leaderboardofusers = await User.findAll({
-            attributes: ['id', 'name', [sequelize.fn('sum', sequelize.col('expenses.expenseamount')), 'total_cost']],
-            include: [
-                {
-                    model: Expense,
-                    attributes: []
-                }
-            ],
-            group: ['user.id'],
-            order: [['total_cost', 'DESC']]
+            
+            order: [['totalExpenses', 'DESC']]
 
         })
 
