160c660cd8a267e4be8ac52de2fcb532ac37aa0a
umeshgrg29 <umeshgrg29@gmail.com>
2023-04-04 07:36:04 +0530

    razorpay integration buy premium membership

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var cors = require('cors')
 const sequelize = require('./util/database');
 const User = require('./models/users');
 const Expense = require('./models/expenses');
+const Order = require('./models/orders');
 
 const userRoutes = require('./routes/user') 
 const expenseRoutes = require('./routes/expense')
+const purchaseRoutes = require('./routes/purchase')
 
 const app = express();
 
@@ -16,10 +18,13 @@ app.use(express.json());
 
 app.use('/user', userRoutes)
 app.use('/expense', expenseRoutes)
+app.use('/purchase', purchaseRoutes)
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
 
+User.hasMany(Order);
+Order.belongsTo(User);
 
 sequelize
     .sync()
